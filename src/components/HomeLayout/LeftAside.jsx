86b78310c46dbd73f6c75d7e2ab869349a143b00
shantunu007 <chakmashantunu.web@gmail.com>
2026-07-13 20:29:41 +0600

    Create categories sidebar in LeftAside

--- src/components/HomeLayout/LeftAside.jsx
+++ src/components/HomeLayout/LeftAside.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+
+
+const categories = [
+    "National",
+    "International",
+    "Sports",
+    "Entertainment",
+    "Culture",
+    "Politics",
+    "Health",
+    "Technology",
+];
 
 const LeftAside = () => {
     return (
-        <div>
-            <h2>left</h2>
-        </div>
+        <aside>
+            <h2 className="text-xl font-bold mb-4">All Category</h2>
+
+            <div className="flex flex-col gap-2">
+                {categories.map((category, index) => (
+                    <button
+                        key={index}
+                        className="btn btn-base-200 justify-start w-full"
+                    >
+                        {category}
+                    </button>
+                ))}
+            </div>
+        </aside>
     );
 };
 
