8e3d211781f731d67950a4c4e195635481a152de
shantunu007 <chakmashantunu.web@gmail.com>
2026-07-17 19:43:57 +0600

    Complete NewsCard with dynamic data and read more functionality

--- src/Pages/CategoryNews.jsx
+++ src/Pages/CategoryNews.jsx
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router";
+import NewsCard from "../components/NewsCard/NewsCard";
 
 
 const CategoryNews = () => {
@@ -7,13 +8,33 @@ const CategoryNews = () => {
     const data = useLoaderData();
     // console.log(typeof (id), data);
 
+    const [categoryNews, setCategoryNews] = useState([]);
+
     useEffect(() => {
-        const filteredNews = data.filter(news => news.category_id == id);
-        console.log(id, filteredNews);
+        if (id == "0") {
+            setCategoryNews(data);
+            return
+        } else if (id == "1") {
+            const filteredNews = data.filter(news => news.others.is_today_pick == true);
+            setCategoryNews(filteredNews);
+        } else {
+            const filteredNews = data.filter(news => news.category_id == id);
+            // console.log(id, filteredNews);
+            setCategoryNews(filteredNews);
+        }
+
     }, [data, id])
     return (
         <div>
-            <h2>Category News {id}</h2>
+            <h2 className="font-bold mb-5">Total <span className="text-secondary">{categoryNews.length}</span> news found</h2>
+
+            <div className="grid grid-cols-1 gap-5">
+                {
+                    categoryNews.map((news) => (
+                        <NewsCard key={news.id} news={news}></NewsCard>
+                    ))
+                }
+            </div>
         </div>
     );
 };
---
+++ src/components/NewsCard/NewsCard.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import { FaEye } from "react-icons/fa6";
+import { FaStar } from "react-icons/fa6";
+import { CiShare2 } from "react-icons/ci";
+import { CiBookmark } from "react-icons/ci";
+
+const NewsCard = ({ news }) => {
+
+    const [showFull, setShowFull] = useState(false);
+    return (
+        <div className="card bg-base-100 shadow-md p-5 rounded-xl">
+            {/* Author */}
+            <div className="flex justify-between items-center">
+
+                {/* Left */}
+                <div className="flex items-center gap-5">
+                    <img
+                        className="w-12 h-12 rounded-full object-cover"
+                        src={news.author.img}
+                        alt=""
+                    />
+
+                    <div>
+                        <h3 className="font-bold">{news.author.name}</h3>
+                        <p className="text-sm text-gray-500">
+                            {new Date(news.author.published_date).toLocaleDateString()}
+                        </p>
+                    </div>
+                </div>
+
+                {/* Right */}
+                <div className="flex items-center gap-3 text-2xl text-gray-500">
+                    <CiBookmark className="cursor-pointer" />
+                    <CiShare2 className="cursor-pointer" />
+                </div>
+
+            </div>
+
+            {/* Title */}
+            <h2 className="text-3xl font-bold my-6 leading-snug">
+                {news.title}
+            </h2>
+
+            {/* Thumbnail */}
+            <img
+                className="w-full rounded-lg"
+                src={news.image_url}
+                alt=""
+            />
+
+            {/* Details */}
+            <div className="mt-6 text-gray-600">
+                <p>
+                    <span className="font-semibold">Wednesday, August 24, 2022</span>
+                    {" | "}
+                    Tags: {news.tags.join(", ")}
+                </p>
+
+                <p className={`mt-4 ${showFull ? "" : "line-clamp-4"}`}>
+                    {news.details}
+                </p>
+                <button onClick={() => setShowFull(!showFull)} className="text-orange-500 font-semibold cursor-pointer">
+                    {" "}
+                    {showFull ? "Show less" : "Read More"}
+                </button>
+            </div>
+
+            {/* Footer */}
+            <div className="border-t mt-6 pt-5 flex justify-between items-center">
+
+                <div className="flex items-center gap-2">
+                    <div className="text-orange-400 flex">
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                    </div>
+
+                    <span className="font-semibold">4.9</span>
+                </div>
+
+                <div className="flex items-center gap-2 text-gray-500">
+                    <FaEye />
+                    <span>{news.total_view}</span>
+                </div>
+
+            </div>
+        </div >
+    );
+};
+
+export default NewsCard;
