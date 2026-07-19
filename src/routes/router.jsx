723bf1573274c3dd891d6a18c725c02c2551d5bb
shantunu007 <chakmashantunu.web@gmail.com>
2026-07-19 11:06:10 +0600

    sticky scroll behaviour implemented

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -19,6 +19,18 @@ const router = createBrowserRouter([
             }
         ]
     },
+    {
+        path: "/auth",
+        element: <h2>Authentication layout</h2>
+    },
+    {
+        path: "/news",
+        element: <h2>news</h2>
+    },
+    {
+        path: "/*",
+        element: <h2>Error 404</h2>
+    }
 ]);
 
 export default router;
