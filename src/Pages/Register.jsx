2173da5d6ed3a567ec02c5b68e364a6eab753c84
shantunu007 <chakmashantunu.web@gmail.com>
2026-07-20 11:50:34 +0600

    set up Firebase config and AuthContext provider

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -9,10 +9,24 @@ const Register = () => {
                 <h2 className="font-semibold text-center text-2xl">Register your account</h2>
                 <div className="card-body">
                     <fieldset className="fieldset">
+                        {/* email */}
+                        <label className="label">Name</label>
+                        <input type="text" className="input" placeholder="Name" />
+
+                        {/* Photo Url */}
+                        <label className="label">Photo URl</label>
+                        <input type="text" className="input" placeholder="Photo URl" />
+
+
+                        {/* Email */}
                         <label className="label">Email</label>
                         <input type="email" className="input" placeholder="Email" />
+
+
                         <label className="label">Password</label>
                         <input type="password" className="input" placeholder="Password" />
+
+
                         <div><a className="link link-hover">Forgot password?</a></div>
                         <button className="btn btn-neutral mt-4">Login</button>
                         <p className="text-center font-semibold">Already have an account? <Link to="/auth/login" className="underline text-orange-500">Login</Link></p>
